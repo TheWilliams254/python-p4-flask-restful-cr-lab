d51d98163e02443af3d99d5975a8ea4752d49f6e
william <williamwambugu663@gmail.com>
2025-06-21 07:40:49 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -17,11 +17,32 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+    
+    def post(self):
+        new_plant = Plant(
+            name=request.json['name'],
+            image=request.json['image'],
+            price=request.json['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return make_response(jsonify(new_plant.to_dict()), 201)
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        return jsonify(plant.to_dict())
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
